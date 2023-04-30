8092141c05e967d7e1db894ba80a793de40b6753
Arthur Fernandez <arthur@snooget.com>
2023-04-30 11:51:50 -0700

    added logo to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import kiaSoulKult from '../assets';
 
 const Header = () => {
-    return <div>Header</div>
+    return <div className = "w-full h-20 bg-white border-b-[1px] border-b-gray-800">
+        <div>
+            <img src={kiaSoulKult} alt="kia soul cult"></img>
+        </div>
+        <div></div>
+        </div>
 };
 
 export default Header; // Exported
