e2fbeeb1647a6d77ad739441e40a29b71f792165
Arthur Fernandez <arthur@snooget.com>
2023-04-30 11:55:23 -0700

    adjusted header height & width

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 import kiaSoulKult from '../assets';
 
 const Header = () => {
-    return <div className = "w-full h-20 bg-white border-b-[1px] border-b-gray-800">
+    return <div className = "w-full h-32 bg-white border-b-[1px] border-b-gray-800">
         <div>
-            <img src={kiaSoulKult} alt="kia soul cult"></img>
+            <img className="w-28" src={kiaSoulKult} alt="kia soul cult"></img>
         </div>
         <div></div>
         </div>
