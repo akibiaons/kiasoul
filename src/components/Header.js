095532887fa74174e31226e34bc768fef700183d
Arthur Fernandez <arthur@snooget.com>
2023-04-30 11:58:37 -0700

    added <ul> formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,18 @@ import kiaSoulKult from '../assets';
 
 const Header = () => {
     return <div className = "w-full h-32 bg-white border-b-[1px] border-b-gray-800">
-        <div>
+        <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
             <img className="w-28" src={kiaSoulKult} alt="kia soul cult"></img>
         </div>
-        <div></div>
+        <div>
+            <ul>
+                <li>Home</li> 
+                <li>Pages</li>
+                <li>Shop</li>
+                <li>Element</li>  
+                <li>Blog</li>              
+            </ul>
+        </div>
         </div>
 };
 
