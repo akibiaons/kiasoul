57c0ecc23b76a68e0c71d8203e3ed5c0c68221d3
Arthur Fernandez <arthur@snooget.com>
2023-04-30 12:31:11 -0700

    added classes to link buttons for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,20 @@ import kiaSoulKult from '../assets';
 
 const Header = () => {
     return (
-        <div className="w-full h-20 bg-white border-b-[1px] border-b-gray-800">
+        <div className="w-full h-34 bg-white border-b-[1px] border-b-gray-800">
             <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
                 <div>
                     <img className="w-28" src={kiaSoulKult} alt="logo"/>
                 </div>
-            </div>
-            <div>
-                <ul>
-                    <li>Home</li>
-                    <li>Pages</li>
-                    <li>Shop</li>
-                    <li>Element</li>
-                    <li>Blog</li>
+                <div>
+                <ul className="flex item-center gap-8">
+                    <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300">Home</li>
+                    <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300">Pages</li>
+                    <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300">Shop</li>
+                    <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300">Element</li>
+                    <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300">Blog</li>
                 </ul>
+            </div>
             </div>
         </div>
     );
