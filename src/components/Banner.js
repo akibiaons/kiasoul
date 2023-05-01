719dd8b6897ada5fc57d40da9609420368e27a7c
Arthur Fernandez <arthur@snooget.com>
2023-04-30 20:59:40 -0700

    added images

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Banner = () => {
     const data = [
-        "", /*Images will go here...*/
-        "",
-        "",
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.prnewswire.com%2Fnews-releases%2Fkia-motors-americas-music-loving-hamsters-shuffle-to-lmfaos-smash-hit-party-rock-anthem-in-new-advertising-campaign-for-funky-soul-urban-passenger-vehicle-128452523.html&psig=AOvVaw3D837Lbhh7uRVOT9XPBOHi&ust=1682999747090000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNDSzOSc0_4CFQAAAAAdAAAAABAD",/*Images will go here...*/
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.autobytel.com%2Fkia%2Fsoul%2F2015%2Fnews%2Ffully-charged-ad-campaign-launches-for-2015-kia-soul-ev-125513%2F&psig=AOvVaw0_otaUV-fpSs6Nb-3y5JH8&ust=1682999798708000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOCXhf2c0_4CFQAAAAAdAAAAABBf",
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thedrive.com%2Fnews%2Fwatch-a-runaway-tire-catapult-a-kia-soul-into-the-air-in-shocking-highway-accident&psig=AOvVaw3kyibjxCHJdzLk7d3UkoSZ&ust=1682999944632000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCICs_sWd0_4CFQAAAAAdAAAAABAD",
     ];
     return <div>Banner</div>;
 };
