d792c519a23fea385872067e9f0108e02a6609df
Arthur Fernandez <arthur@snooget.com>
2023-04-30 20:34:46 -0700

    created banner module and set import to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Banner from "../components/Banner";
 
 const Home = () => {
-    return <div>Home</div>
+    return <div>
+        <Banner />
+    </div>
 };
 
 export default Home;
