2dd82abd11a3574442f03116e701f90a74897874
Arthur Fernandez <arthur@snooget.com>
2023-04-30 21:52:07 -0700

    adding slide function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className='font-bodyFont'>
       <Header />
+      <Home />
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,15 +2,18 @@ import React from "react";
 
 const Banner = () => {
     const data = [
-        "https://www.prnewswire.com/news-releases/kia-motors-americas-music-loving-hamsters-shuffle-to-lmfaos-smash-hit-party-rock-anthem-in-new-advertising-campaign-for-funky-soul-urban-passenger-vehicle-128452523.html#",/*Images will go here...*/
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.autobytel.com%2Fkia%2Fsoul%2F2015%2Fnews%2Ffully-charged-ad-campaign-launches-for-2015-kia-soul-ev-125513%2F&psig=AOvVaw0_otaUV-fpSs6Nb-3y5JH8&ust=1682999798708000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOCXhf2c0_4CFQAAAAAdAAAAABBf",
-        "https://www.prnewswire.com/news-releases/kia-motors-americas-music-loving-hamsters-shuffle-to-lmfaos-smash-hit-party-rock-anthem-in-new-advertising-campaign-for-funky-soul-urban-passenger-vehicle-128452523.html#",
+        "src/assets/img-0.jpeg",
+        "src/assets/img-1.png",
+        "src/assets/img-2.png",
     ];
     return (
         <div className="w-full h-auto">
             <div>
                 <div>
-                    <img src={data[0]} alt="ImgOne"/>
+                    <img
+                    className="w-screen h-full object-cover"
+                    src={data[0]} 
+                    alt="deals!"/>
                 </div>
             </div>
         </div>
