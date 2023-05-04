e9bfce1a44567e6fd39c90670b23fe44e0d4f463
Arthur Fernandez <arthur@snooget.com>
2023-05-03 21:00:55 -0700

    added the Products module

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../components/Banner";
+import Product from "../components/Products";
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Product />
         </div>
     );
 };
