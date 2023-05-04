6f14391636f4b582c2a143a56149fef5990131cf
Arthur Fernandez <arthur@snooget.com>
2023-05-03 18:22:29 -0700

    added slide function, but is buggy

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,15 +2,24 @@ import React from "react";
 import { HiArrowRight, HiArrowLeft } from "react-icons/hi";
 
 const Banner = () => {
+        const [currentSlide, setCurrentSlide]= useState(0);
     const data = [
         'https://raw.githubusercontent.com/akibiaons/kiasoul/main/src/assets/img-0.jpeg',
         "https://raw.githubusercontent.com/akibiaons/kiasoul/main/src/assets/img-1.png",
         "https://raw.githubusercontent.com/akibiaons/kiasoul/main/src/assets/img-2.png",
     ];
+
+    const prevSlide = () => {
+        setCurrentSlide(currentSlide === 0 ? 3 : (prev) => prev - 1);
+    };
+    const nextSlide = () => {
+        setCurrentSlide(currentSlide === 3 ? 0 : (prev) => prev + 1);
+    };
+
     return (
         <div className="w-full h-auto overflow-x-hidden">
             <div className="w-screen h-[650px] relative">
-                <div className="w-[400vw] h-full flex">
+                <div style={{transform:`translateX(-${currentSlide * 100}vw)`}} className="w-[400vw] h-full flex">
                     <img
                     className="w-screen h-full object-cover"
                     src={data[0]} 
@@ -30,10 +39,10 @@ const Banner = () => {
                 </div>
                 {/*below are the arrows for the home page img carosel*/}
                 <div className="absolute w-fit left-0 right-0 mx-auto flex gap-8 bottom-44">
-                    <div onClick="{prevSlide}" className="w-14 h-12 border-[1px] border-gray-700 flex items-center justify-center hover:cursor-pointer hover:bg-gray-700 hover:text-white active:bg-gray-900 duration-300">
+                    <div onClick={prevSlide} className="w-14 h-12 border-[1px] border-gray-700 flex items-center justify-center hover:cursor-pointer hover:bg-gray-700 hover:text-white active:bg-gray-900 duration-300">
                         <HiArrowLeft />
                     </div>
-                    <div onClick="{nextSlide}" className="w-14 h-12 border-[1px] border-gray-700 flex items-center justify-center hover:cursor-pointer hover:bg-gray-700 hover:text-white active:bg-gray-900 duration-300">
+                    <div onClick={nextSlide} className="w-14 h-12 border-[1px] border-gray-700 flex items-center justify-center hover:cursor-pointer hover:bg-gray-700 hover:text-white active:bg-gray-900 duration-300">
                         <HiArrowRight />
                     </div>
                 </div>
