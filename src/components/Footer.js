d915364379c5a9e97fbed3201dd4e20bfc562751
Arthur Fernandez <arthur@snooget.com>
2023-05-06 09:21:43 -0700

    imported social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import {kiaSoulKult, cartImg} from "../assets/index";
+import {
+    FaFacebookF,
+    FaTwitter,
+    FaInstagram,
+    FaYoutube,
+    FaHome,
+} from "react-icons/fa";
+import { logoLight, paymentLogo } from "../assets";
 
 const Footer = () => {
   return (
     <div className="bg-black text-[#949494] py-20 font-titleFont">
         <div className="max-w-screen-xl mx-auto">
             {/* ======== LogoIcon Start here ============ */}
-            <div> {/* This <div> wil have footer logo which is light to contrast with dark-bg*/}
-                <img className="w-32" src={kiaSoulKult} alt="logoLight"/>
+            <div className="flex flex-col gap-7"> {/* This <div> wil have footer logo which is light to contrast with dark-bg*/}
+                <img className="w-32" src={kiaSoulKult} alt="logoLightGoesHere"/>
+                <p className="text-white text-sm tracking-wide">ReactBD.com</p>
+                <img className="w-56" src="" alt="paymentLogoGoesHere"/>
             </div>
             {/* ======== LogoIcon End here ============ */}
             {/* ======== LocateUs Start here ========== */}
