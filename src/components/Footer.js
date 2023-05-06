da43db1180a4db206c569f9b61c833ab747e4ce6
Arthur Fernandez <arthur@snooget.com>
2023-05-06 08:33:34 -0700

    added footer logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="bg-black text-[#949494] py-20 font-titleFont">Footer</div>
+    <div className="bg-black text-[#949494] py-20 font-titleFont">
+        <div className="max-w-screen-xl mx-auto">
+            {/* ======== LogoIcon Start here ============ */}
+            <div> {/* This <div> wil have footer logo which is light to contrast with dark-bg*/}
+                <img className="w-32" src={logoLight} alt="logoLight"/>
+            </div>
+            {/* ======== LogoIcon End here ============ */}
+            {/* ======== LocateUs Start here ========== */}
+            {/* ======== LocateUs End here ========== */}
+            {/* ======== Profile Start here ========== */}
+            {/* ======== Profile End here ========== */}
+            {/* ======== Subscribe Start here ========== */}
+            {/*========= Subscribe End here ==========*/}
+        </div>
+    </div>
   )
 }
 
