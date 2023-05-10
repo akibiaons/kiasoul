1cc85fb388f52f49a2da788882799feb02a01693
Arthur Fernandez <arthur@snooget.com>
2023-05-09 22:15:52 -0700

    added footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -44,33 +44,39 @@ const Footer = () => {
             {/* ======== Profile Start here ========== */}
             <div>
                 <h2 className="text-2xl font-semibold text-white mb-4">Profile</h2>
-                <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
-                    <span>
-                        <BsPersonFill />
-                    </span>
-                    My account
-                </p>
-                <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
-                    <span className="text-lg">
-                        <BsPaypal />
-                    </span>
-                    Checkout
-                </p>
-                <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
-                    <span className="text-lg">
-                        <FaHome />
-                    </span>
-                    Order Tracking
-                </p>
-                <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
-                    <span className="text-lg">
-                        <MdLocationOn />
-                    </span>
-                    Location
-                </p>
+                <div className="flex flex-col gap-2 text-base">
+                    <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+                        <span>
+                            <BsPersonFill />
+                        </span>
+                        My account
+                    </p>
+                    <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+                        <span className="text-lg">
+                            <BsPaypal />
+                        </span>
+                        Checkout
+                    </p>
+                    <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+                        <span className="text-lg">
+                            <FaHome />
+                        </span>
+                        Order Tracking
+                    </p>
+                    <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+                        <span className="text-lg">
+                            <MdLocationOn />
+                        </span>
+                        Location
+                    </p>
+                </div>
             </div>
             {/* ======== Profile End here ========== */}
             {/* ======== Subscribe Start here ========== */}
+            <div className="flex flex-col justify-center">
+                <input className="bg-transparent border px-4 py-2 text-sm" placeholder="e-mail" type="text"/>
+                <button className="text-sm border text-white border-t-0 hover:bg-gray-900 active:bg-white active:text-black">Subscribe</button>
+            </div>
             {/*========= Subscribe End here ==========*/}
         </div>
     </div>
