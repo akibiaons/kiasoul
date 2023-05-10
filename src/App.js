e11897200dbba98a2dbff825c700b151c2458f70
Arthur Fernandez <arthur@snooget.com>
2023-05-09 22:26:26 -0700

    added layout function

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ import {
   ScrollRestoration,
 } from "react-router-dom";
 
+// Will create a layout below to define what I want. So Header and footer will show on every page when I click on cart or checkout for example.
+const layout =()=>{
+  return(
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
 function App() {
   return (
     <div className='font-bodyFont'>
